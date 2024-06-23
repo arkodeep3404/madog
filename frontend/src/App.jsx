bf83a49e761c16ba20019befffa1a65d209b4b96
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-06-23 23:37:31 +0530

    added signup, signin pages, and toast  :p

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { RecoilRoot } from "recoil";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
 import Editor from "./pages/editor";
+import Signup from "./pages/signup";
+import Signin from "./pages/signin";
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/editor" element={<Editor />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <div>
+      <Toaster />
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
   );
 }
